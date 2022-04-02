ab176c60d99c3c67f60034cc64bfdd1e7cee6d1f
Kleber de Souza <kleber107@gmail.com>
2022-04-02 18:00:03 -0300

    Crontab

---
+++ History/3e2e0403/9xLI.js
@@ -0,0 +1,40 @@
+const {validate, sizeOfCPFHas11Digits, allNumbersAreEqual} = require("./index.js");
+
+const cpfValid = "11111111111";
+const arEqual = allNumbersAreEqual(cpfValid);
+
+test("CPF deve ser valido", () => {
+	const cpfValid = "704.557.190-98";
+	const isValid = validate(cpfValid);
+	expect(isValid).toBe(true);
+});
+
+test("Deve ser NULL", () => {
+	const cpfValid = "";
+	const isInvalid = validate(cpfValid);
+	expect(isInvalid).toBe(false);
+});
+
+test("CPF deve ser inválido", () => {
+	const cpfValid = "123.456.789-00";
+	const isInvalid = validate(cpfValid);
+	expect(isInvalid).toBe(false);
+});
+
+test("Todos os números são iguais", () => {
+	const cpfValid = "11111111111";
+	const arEqual = allNumbersAreEqual(cpfValid);
+	expect(arEqual).toBe(true);
+});
+
+test("Validar CPF - Todos os números são iguais", () => {
+	const cpfValid = "11111111111";
+	const arEqual = validate(cpfValid);
+	expect(arEqual).toBe(false);
+});
+
+test("Deve dar erro se o CPF tiver menos ou mais do que 11 caracteres", () => {
+	const cpfValid = "123.456.7812-00";
+	const isInvalid = sizeOfCPFHas11Digits(cpfValid);
+	expect(isInvalid).toBe(false);
+});
--- History/3e2e0403/YStu.js
+++
@@ -1,7 +0,0 @@
-const example2 = require("./example2_after.js");
-
-test("Deve validar o cpf", () => {
-	const cpfValid = "531.876.690-00"
-	const isValid = example2.validate(cpfValid);
-	expect(isValid).toBe(true);
-});
